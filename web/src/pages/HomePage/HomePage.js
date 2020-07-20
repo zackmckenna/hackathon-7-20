50d9696eeca7a9ec4169d44c616acfe620d3a990
Zack McKenna <zackmckenna@gmail.com>
2020-07-20 14:03:39 -0400

    key logging function

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,10 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Parallax from 'parallax-js'
 
 const HomePage = () => {
+  const [pressedLetter, setPressedLetter] = useState(null)
+
   useEffect(() => {
     const scene = document.getElementById('scene')
     const parallaxInstance = new Parallax(scene)
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener('keydown', logKey)
+
+    function logKey(e) {
+      console.log(e.code.slice(3))
+      setPressedLetter(e.code.slice(3))
+    }
   }, [])
 
   const createLayer = (depth, contents) => {
@@ -16,25 +27,34 @@ const HomePage = () => {
   }
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <ul
-        className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
-        id="scene"
-        data-friction-x="0.1"
-        data-friction-y="0.1"
-        data-scalar-x="25"
-        data-scalar-y="15"
-      >
-        {createLayer('2.0', () => (
-          <div>test2</div>
-        ))}
-        <li className="layer" data-depth="2.0">
-          <div>test 4</div>
-        </li>
-        {createLayer('4.0', () => (
-          <div>test3</div>
-        ))}
-      </ul>
+    <div>
+      <input></input>
+      <div className="flex items-center justify-center h-screen">
+        <ul
+          className="items-center justify-center font-bold text-6xl rounded-lg p-10"
+          id="scene"
+          data-friction-x="0.1"
+          data-friction-y="0.1"
+          data-scalar-x="50"
+          data-scalar-y="50"
+        >
+          <li className="layer" data-depth="0.0">
+            <div></div>
+          </li>
+          <li className="layer" data-depth="1.0">
+            <div>M</div>
+          </li>
+          <li className="layer" data-depth="3.0">
+            <div>E</div>
+          </li>
+          <li className="layer" data-depth="4.0">
+            <div>G</div>
+          </li>
+          <li className="layer" data-depth="5.0">
+            <div>A</div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
